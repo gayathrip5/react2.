3b449ccd6d065340b2f3a820a0f4f345dc2ef4d4
GayathriPirikiti <106241448+gayathrip5@users.noreply.github.com>
2022-07-16 17:07:53 +0530

    Create 3)function components.js

---
+++ 3)function components.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Welcome=()=>
+{
+return (
+	<h1>functions example in reactjs</h1>
+	);
+}
+
+const functionExample=()=>
+{
+	return (
+	<Welcome/>
+	);
+}
+
+export default functionExample;
